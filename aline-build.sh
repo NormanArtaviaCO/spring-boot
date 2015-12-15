29e49e897de073f008075678e8828ec549e71d23
Himanshu Khurana <himanshu@yoatm.com>
2015-12-15 19:33:57 +0400

    fixed the artifacts upload path

--- aline-build.sh
+++ aline-build.sh
@@ -1,2 +1,6 @@
-mvn install -q -nsu -Dmaven.test.redirectTestOutputToFile=true -P '!integration' -DskipTests=true;
-echo "------"
+echo "Building"
+mvn install -q -nsu -Dmaven.test.redirectTestOutputToFile=true -P '!integration' -DskipTests
+
+#rm -rf aline-artifacts
+#mkdir -p aline-artifacts
+#cp -r $(git ls-files -o --directory | tr "\n" ";") aline-artifacts
