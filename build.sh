ed9e1c7b07f067a57ab2e57a566e63e753bc4e6f
rahulsub <rahulsub@Rahuls-MacBook-Pro.local>
2015-12-15 18:45:25 +0400

    added build.sh and debug data

--- build.sh
+++ build.sh
@@ -1,5 +1,2 @@
 mvn install -q -nsu -Dmaven.test.redirectTestOutputToFile=true -P '!integration' -DskipTests=true;
-export ARTIFACTS_PATHS="$(git ls-files -o | tr \"\\n\" \":\")"
-echo $ARTIFACTS_PATHS
 echo "------"
-cat /home/travis/build.sh
