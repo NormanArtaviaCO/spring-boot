72e96df31fcfcab194a2a5a16446fe3198ba312e
Himanshu Khurana <himanshu@yoatm.com>
2015-12-19 03:37:59 +0400

    Coveralls Integration

--- aline-build.sh
+++ aline-build.sh
@@ -1,10 +1,10 @@
 echo "Building..."
-mvn install jacoco:report coveralls:report -q -nsu -Dmaven.test.redirectTestOutputToFile=true -P '!integration'
+mvn install -q -nsu -Dmaven.test.redirectTestOutputToFile=true -P '!integration'
 echo "Copying artifacts..."
 rm -r aline-artifacts.zip
 zip aline-artifacts.zip $(git ls-files -o)
 echo "Build Done!"
-#mvn -e test jacoco:report coveralls:report
+mvn -e test jacoco:report coveralls:report
 echo "Testing Done!"
 
 
