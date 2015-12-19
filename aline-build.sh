08120de1a823aeb82b63e7d8a9263c63760a1c10
Himanshu Khurana <himanshu@yoatm.com>
2015-12-19 15:45:35 +0400

    using after_success tag

--- aline-build.sh
+++ aline-build.sh
@@ -1,9 +1,7 @@
 echo "Building..."
-mvn install jacoco:report coveralls:jacoco -q -nsu -Dmaven.test.redirectTestOutputToFile=true -P '!integration'
+mvn install -q -nsu -Dmaven.test.redirectTestOutputToFile=true -P '!integration'
 echo "Copying artifacts..."
 rm -r aline-artifacts.zip
 zip aline-artifacts.zip $(git ls-files -o)
 echo "Build Done!"
-#mvn -e test jacoco:report coveralls:report
-
-
+#mvn -e jacoco:report coveralls:report
