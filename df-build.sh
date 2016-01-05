d71c072bdb2be7942b70018260e146fb0582bc9e
Himanshu Khurana <himanshukhurana@Himanshus-MacBook-Pro.local>
2016-01-05 12:46:47 +0400

    Instrumented by Insights Service for uploading the artifacts

---
+++ df-build.sh
@@ -0,0 +1,10 @@
+echo "Build is done by now. Copying dependencies..."
+mvn dependency:copy-dependencies
+echo "Cleaning up existing zip if any..."
+rm -f libs.zip
+rm -f build.zip
+echo "Zipping artifacts begin..."
+zip libs.zip $(git ls-files -o | grep -e target/dependency/.*jar)
+zip build.zip $(git ls-files -o | grep -e target/classes/.*class)
+echo "Zipping artifacts DONE!!!"
+# Modified by Insights Service at 2016-01-05 12:46:07.066345
