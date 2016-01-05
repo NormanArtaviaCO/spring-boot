45aa7ee7bfc187be12dddcd98018b1cad9e578b4
Himanshu Khurana <himanshukhurana@Himanshus-MacBook-Pro.local>
2016-01-05 13:06:01 +0400

    Instrumented by Insights Service for uploading the artifacts

--- df-build.sh
+++ df-build.sh
@@ -7,4 +7,4 @@ echo "Zipping artifacts begin..."
 zip libs.zip $(git ls-files -o | grep -e target/dependency/.*jar)
 zip build.zip $(git ls-files -o | grep -e target/classes/.*class)
 echo "Zipping artifacts DONE!!!"
-# Modified by Insights Service at 2016-01-05 12:46:07.066345
+# Modified by Insights Service at 2016-01-05 13:05:17.358054
